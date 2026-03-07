019067bc4df19010bbdb213e67f469f13a9e7f2f
syed-ali3 <aliahmad4142@gmail.com>
2026-03-07 22:09:21 +0500

    [BACKEND] DB sql added

---
+++ backend/db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    email VARCHAR(255) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL,
+    github_username VARCHAR(100),
+    linkedin_username VARCHAR(100),
+    skills TEXT[],          
+    is_verified BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE otp_verifications (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) REFERENCES users(email) ON DELETE CASCADE,
+    otp_code VARCHAR(10) NOT NULL,
+    expires_at TIMESTAMP NOT NULL
+);
